88412d2ce3e58c60b92833b3e363542fde33629f
Soham <sohamgangurde1@gmail.com>
2024-11-06 01:17:58 +0530

    solve exercise 5
    This one was easy after tackling errors like checking if it's a number or not and then just summing up using loops

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    if(Number.isInteger(num1)===false || num1<=0 || Number.isInteger(num2) ===false || num2<=0 ){
+        return "ERROR";
+    }
+    let sum = 0;
+    let min,max;
+    if(num1 < num2){
+        min = num1;
+        max = num2;
+    }
+    else{
+        min = num2;
+        max = num1;
+    }
+    for(i = min;i<=max;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
