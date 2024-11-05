71b839b048d654774b90097d6e1a29b9f5a4b16a
Soham <sohamgangurde1@gmail.com>
2024-11-05 23:22:24 +0530

    solve 2nd exercise
    I learned how test.skip() is used to skip some tests.
    Made changes according to specifications in the repeatString.spec.js file,and then also changed repeatString.js file in order to pass all tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text,number) {
+    if(number < 0) return "ERROR";
+    let result = "";
+    for(let i = 0;i<number;i++){
+        result += text;
+    }
+    return result;
 };
 
 // Do not edit below this line
