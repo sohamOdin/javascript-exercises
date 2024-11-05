8cdbcc1c679ab0d5e8309425c878cd6a545c6194
Soham <sohamgangurde1@gmail.com>
2024-11-06 02:39:10 +0530

    solve no.4 removeFromArray exercise
    This one was the most frustrating bcoz of my lack of focus.
    What I learned here is the use of forEach method and the !args.includes(item) method.
    The crux of the problem was that if the original array's element doesn't exists in the rest parameters.Then simply add main array's items to a new array confirming that they need not be removed.
    Thus,the 2 loops work.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,...args) {
+    const result = [];
+    let i = 0;
+    array.forEach((item) => {
+        if(!args.includes(item)){
+            result.push(item);
+        }
+    });
+    return result;
+};//1,2,3,4 ,, 3,4
 
 // Do not edit below this line
 module.exports = removeFromArray;
