1fb518b78456540110ebed98dca547e5e24bd0c7
Soham <sohamgangurde1@gmail.com>
2024-11-28 19:13:24 +0530

    To solve all exercises from object basics.
    This was the last exercise and tbh I mostly did it by looking at solutions.
    But that's fine as I learned from them thoroughly.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const newStr = str.toLowerCase().replace(/[^a-z0-9]/g,'');
+    const reverse = newStr.split('').reverse().join('');
+    return reverse === newStr;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    return people.reduce((oldest,person)=>{
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const currentAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+
+        return currentAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
